ca68d9ed647087816e140738b13d870a3fafffe4
tasiclazar01 <119966453+tasiclazar01@users.noreply.github.com>
2022-12-17 17:36:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,64 +44,65 @@ img_contract_form3 = Image.open("images/slika2.png")
 img_lottie_animation3 = Image.open("images/slika2.png")
 
 
-st.subheader("Zdravo, ja sam Djordje :wave:")
-st.title("Junior električar iz Srbije")
-st.write("Elektrika je moja strast. Elektronika je moja strast. Uzivam u radu sa malim i velikim strujama.")
-st.write("Moj instagram > https://www.instagram.com/djordjetasic/")
+st.subheader("Здраво, ја сам Ђорђе :wave:")
+st.title("Јуниор електричар из Житорађа")
+st.write("Електрика је моја страст. Електроника је моја страст. Уживам у раду са великим и малим струјама.")
+st.write("Мој инстаграм > https://www.instagram.com/djordjetasic/ :selfie:")
 
 with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.header("Šta ja radim?")
+        st.header("Чиме се све бавим?")
         st.write("##")
         st.write(
             """
-            Iako sam junior električar bavim se sa više različitih stvari:
-            - instalacija strujne mreže u kući
-            - popravka svih vrsta bele tehnike
-            - popravka televizora, računara, laptopova
-            - popravka elektronskih komponenti na matičnim pločama
-            - instalacija led traka po želji
+            Иако сам јуниор електричар пуно тога већ радим:
+            - инсталација струјне мреже у кући
+            - поправка свих врста беле технике
+            - поправка телевизора, рачунара и лаптопова
+            - поправка електронских компоненти на матичним плочама
+            - инсталација лед трака по жељи
             """
         )
-        st.write("Ja sam vaše svetlo na kraju tunela :flashlight:")
+        st.write("ЈА САМ ВАШЕ СВЕТЛО НА КРАЈУ ТУНЕЛА :flashlight:")
     with right_column:
         st_lottie(lottie_coding, height=300, key = "coding")
 
 with st.container():
     st.write("---")
-    st.header("Moj najveći projekat u inostranstvu")
+    st.header("Неки од највећих послова до сад :arrow_down:")
     st.write("##")
     image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image(img_lottie_animation)
 
     with text_column:
-        st.subheader("Instalacija kod Putina")
+        st.subheader("Хотел на Златибору")
         st.write(
             """
-            Nema Rusija elektricara kao sto ga ima Srbija.
-            Pozvan sam od strane KGB da sprovedem celu unutrašnju instalaciju.
-            Šta da kažem, ja sam čovek od poverenja :)
+            Рад у овом хотелу је један од највећих
+            задатака које сам имао досад. У питању
+            је велики пројекат и драго ми је да сам
+            имао учешће у томе. 
             """
         )
 
 with st.container():
     st.write("---")
-    st.header("Moji najveći projekat u Srbiji")
+    st.header("Уметност са лед тракама :arrow_down:")
     st.write("##")
     image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image(img_lottie_animation2)
 
     with text_column:
-        st.subheader("Instalacija kod Destana")
+        st.subheader("Мало маште и резултат као из бајке")
         st.write(
             """
-            Destan radi u Nemačku i ne zna kolko pare ima. Celo selo se čudi odokle mu.
-            Kuća mu liči na svemirski brod, svi joj se dive. 
-            To sam ja s mojih deset prstiju sve odradio.
+            У питању је приватна кућа на којој сам дуго радио.
+            Посветио сам доста времена, али труд се на крају 
+            исплатио као што се и види из приложеног.
             """
         )
 
@@ -109,7 +110,7 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.header("Pesma za sve kolege električare")
+        st.header("А ко пева зло не мисли! Послушајте химну наше струке :zap:")
         st.write("##")
         audio_file = "muzika/audio.mp3"
 
@@ -120,16 +121,16 @@ with st.container():
 
 with st.container():
     st.write("---")
-    st.header("Kontaktirajte me i zakažite popravku!")
+    st.header("Закажите своју поправку већ сада!")
     st.write("##")
 
     contact_form="""
     <form action="https://formsubmit.co/lazartasic05@gmail.com" method="POST">
      <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Vaše ime" required>
-     <input type="email" name="email" placeholder="Vaš imejl" required>
-     <textarea name="message" placeholder="Ovde ostavite vašu poruku" required></textarea> 
-     <button type="submit">Send</button>
+     <input type="text" name="name" placeholder="Ваше име" required>
+     <input type="email" name="email" placeholder="Ваша имејл адреса" required>
+     <textarea name="message" placeholder="Овде оставите Вашу поруку" required></textarea> 
+     <button type="submit">Пошаљи</button>
     </form>
     """
 
